8ddec38ba63c1c379cfbdd6b8730f3f39cac4e13
Vasily Gotovko <vasily.gotovko@gmail.com>
2013-08-22 17:55:32 +0300

    Display time_entry in the journal

--- lib/redmine_better_journal/hooks.rb
+++ lib/redmine_better_journal/hooks.rb
@@ -1,5 +1,10 @@
 module RedmineBetterJournal
   class Hooks < Redmine::Hook::ViewListener
+
+    # display time entry in journal
+    render_on :view_issues_history_journal_bottom, :partial => 'better_journal_show_issue_patch'
+
+    # save reference between time_entry and journal
     def controller_issues_edit_after_save(context={ })
       time_entry = context[:time_entry]
       journal = context[:journal]
@@ -7,5 +12,6 @@ module RedmineBetterJournal
       time_entry.journal = journal
       time_entry.save
     end
+
   end
 end
